99b816adf1957b61b11e8d0461de7a77baa9e25c
ketaki001 <81285716+ketaki001@users.noreply.github.com>
2023-05-04 19:15:44 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -115,7 +115,7 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
+      schoolName: "MIT ACADEMY OF ENGINEERING",
       logo: require("./assets/images/harvardLogo.png"),
       subHeader: "Master of Science in Computer Science",
       duration: "September 2017 - April 2019",
